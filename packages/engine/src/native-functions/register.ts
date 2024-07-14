7e67ed33c6b781e7b4d6fa6956037242cabb34d4
Zhang Yang <freeyourmind@126.com>
2024-07-14 10:23:41 +0800

    Update register.ts

--- packages/engine/src/native-functions/register.ts
+++ packages/engine/src/native-functions/register.ts
@@ -1,8 +1,10 @@
 import { SimpleNodeNativeComputeFunction } from '../types';
 import { simpleNodeNativeCompute2NodeNativeCompute } from '../utils';
 
-const registerCompute: SimpleNodeNativeComputeFunction = () => {
-  return {};
+const registerCompute: SimpleNodeNativeComputeFunction = ({ inputs }) => {
+  return {
+    out: inputs.in,
+  };
 };
 
 export default () => simpleNodeNativeCompute2NodeNativeCompute(registerCompute);
